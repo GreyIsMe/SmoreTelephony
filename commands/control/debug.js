9e2cfb29be825cb684eb70ee9eabc45ff47b735f
Chronomly <chronomly@gmail.com>
2018-01-15 12:59:50 -0500

    fix?22

--- commands/control/debug.js
+++ commands/control/debug.js
@@ -36,7 +36,7 @@ module.exports = class DebugCommand extends commando.Command {
   }
 
   //eslint-disable-next-line class-methods-use-this
-  async run(message, args) {
+  async run(msg, args) {
     if (args.guild.toLowerCase() === 'local') {
       let guild = msg.guild;
       let announcements = guild.settings.get('announcements')
@@ -47,18 +47,18 @@ module.exports = class DebugCommand extends commando.Command {
       if (number === undefined) number = 'not set'
       if (numberChanID === undefined) numberChanID = 'not set'
 
-      message.reply(`__**Guild Info**__
-**Guild**: ${message.guild.id}
-**Name**: ${message.guild.name}
-**Owner**: ${message.guild.owner.user.tag} (${message.guild.owner.id})
-**Created At:** ${message.guild.createdAt}
-**Members**: ${message.guild.members.size}
-**Bots**: ${message.guild.members.filter(u => u.user.bot).size} (${Math.floor(message.guild.members.filter(u => u.user.bot).size / message.guild.members.size * 100)}%)
-**Humans**: ${message.guild.members.filter(u => !u.user.bot).size} (${Math.floor(message.guild.members.filter(u => !u.user.bot).size / message.guild.members.size * 100)}%)
-**Text Channels**: ${message.guild.channels.filter(channel => channel.type === 'text').size}
-**Voice Channels**: ${message.guild.channels.filter(channel => channel.type === 'voice').size}
-**Default Channel**: ${message.guild.defaultChannel}
-**Roles**: ${message.guild.roles.size}
+      msg.reply(`__**Guild Info**__
+**Guild**: ${msg.guild.id}
+**Name**: ${msg.guild.name}
+**Owner**: ${msg.guild.owner.user.tag} (${msg.guild.owner.id})
+**Created At:** ${msg.guild.createdAt}
+**Members**: ${msg.guild.members.size}
+**Bots**: ${msg.guild.members.filter(u => u.user.bot).size} (${Math.floor(msg.guild.members.filter(u => u.user.bot).size / msg.guild.members.size * 100)}%)
+**Humans**: ${msg.guild.members.filter(u => !u.user.bot).size} (${Math.floor(msg.guild.members.filter(u => !u.user.bot).size / msg.guild.members.size * 100)}%)
+**Text Channels**: ${msg.guild.channels.filter(channel => channel.type === 'text').size}
+**Voice Channels**: ${msg.guild.channels.filter(channel => channel.type === 'voice').size}
+**Default Channel**: ${msg.guild.defaultChannel}
+**Roles**: ${msg.guild.roles.size}
 
 __**Settings**__
 **Global announcements**: "${announcements}"
@@ -75,7 +75,7 @@ __**Settings**__
       if (number === undefined) number = 'not set'
       if (numberChanID === undefined) numberChanID = 'not set'
 
-      message.reply(`__**Guild Info**__
+      msg.reply(`__**Guild Info**__
 **Guild**: ${guild.id}
 **Name**: ${guild.name}
 **Owner**: ${guild.owner.user.tag} (${guild.owner.id})
